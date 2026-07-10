2aad07715f10c968f5845a0963ada79b91420346
Luis-Soft-exe <Luis.Soft.exe@gmail.com>
2026-07-10 16:10:36 -0600

    Update app.py

--- app.py
+++ app.py
@@ -194,7 +194,8 @@ if categoria == "Caballero":
         2: ("Guess Regular Straight Hason", "Medio", "32", "32", "Regular Straight"),
         3: ("Guess Slim Straight", "Negro", "33", "32", "Slim Straight"),
         4: ("Guess Slim Tapered", "Claro", "34", "32", "Slim Tapered"),
-        5: ("Guess Regular Straight", "Medio", "32", "32", "Regular Straight")
+        5: ("Guess Regular Straight", "Medio", "32", "32", "Regular Straight"),
+        6: ("Guess Slim Tapered", "Medio", "36", "32", "Slim Tapered")
     }
 
     vendidos_caballero = [2, 3, 4]
