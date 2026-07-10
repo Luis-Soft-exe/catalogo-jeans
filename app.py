7b4b895020881847d008dede4dc34396b66ee5a6
Luis-Soft-exe <Luis.Soft.exe@gmail.com>
2026-07-10 15:34:11 -0600

    Update app.py

--- app.py
+++ app.py
@@ -242,7 +242,7 @@ elif categoria == "Dama":
         3: ("Guess Mom Low Rise Slouchy", "Medio", "28", "30", "Mom Slouchy"),
         4: ("Guess Low Rise Slouchy", "Medio", "29", "30", "Low Rise Slouchy"),
         5: ("Guess Sexy Boot", "Oscuro", "25", "30", "Sexy Boot"),
-        6: ("Guess Sexy Boot", "Claro", "26", "30", "Sexy Boot")
+        6: ("Guess Sexy Boot", "Claro", "26", "30", "Sexy Boot"),
         7: ("Guess 1981 Skinny ", "Medio", "24", "30", "Skinny")
     }
 
