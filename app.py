18736f9d32da7d777bd6cedc79a01268023a7398
Luis-Soft-exe <Luis.Soft.exe@gmail.com>
2026-07-10 15:59:07 -0600

    Update app.py

--- app.py
+++ app.py
@@ -245,7 +245,8 @@ elif categoria == "Dama":
         6: ("Guess Sexy Boot", "Claro", "26", "30", "Sexy Boot"),
         7: ("Guess 1981 Skinny ", "Medio", "24", "30", "Skinny"),
         8: ("Guess Low Rise Slouchy", "Medio", "26", "30", "Low Rise Slouchy"),
-        9: ("Guess Sexy Boot", "Medio", "26", "30", "Sexy Boot")
+        9: ("Guess Sexy Boot", "Medio", "26", "30", "Sexy Boot"),
+        10: ("Guess Power Curvy Mid", "Claro", "30", "30", "Power Curvy Mid")
     }
 
     vendidos_dama = [3]
